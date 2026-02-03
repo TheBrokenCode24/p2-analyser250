19b67f06c7f4873bdd690c357b826a7a8b5ee9cc
Jimmy Nguyen <jtngyen5@memphis.edu>
2026-02-03 14:38:08 -0600

    Changed the name of ranking.py to rankings.py and updated main.py so that it imports rankings instead of ranking.

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ This is the main program where the user will analyze the ranking sheet.
 """
 
 # Imports the ranking and list of countries to this program.
-from ranking import ranking_2023, ranking_2024, ranking_2025, countries_2023, countries_2024, countries_2025, years_2023, years_2024, years_2025, ranking_1732_2023, ranking_1769_2024, ranking_1806_2025, countries, users2023, users2024
+from rankings import ranking_2023, ranking_2024, ranking_2025, countries_2023, countries_2024, countries_2025, years_2023, years_2024, years_2025, ranking_1732_2023, ranking_1769_2024, ranking_1806_2025, countries, users2023, users2024
 
 # Global variables for all functions to use.
 num_entries = {2023: 1732, 2024: 1769, 2025: 1806}
 
 # Function prints out the entire ranking based on averages from all users.
 def entire_ranking(edition, all_rankings=[ranking_2023, ranking_2024, ranking_2025]):
-    # Gets the ranking depending on the edition
+    # Gets the ranking depending on the edition.
     if edition == 2023:
         ranking = all_rankings[0]
     elif edition == 2024:
@@ -94,26 +94,30 @@ def main():
     print(f"What would you like to get from the ranking of ALTESC250 {edition}?\n")
     
     # Choices
+    # 1. List of the top 250 of the ranking of all users.
+    # 2. List of the top 250 of the ranking of all users who ranked all songs.
+    # 3. List of the top 250 of the ranking of a specific user.
+    print("1 - List of the top 250.")
     # 1. List of the entire ranking. (ranking.csv)
     # 2. List of the entire ranking from those who ranked all the songs (club_1769.csv).
     # 3. List of the entire ranking from a specific user (ranking.csv).
-    print("1 - List of the entire ranking.")
+    print("2 - List of the entire ranking.")
     # 4. All years ranked by average.
     # 5. All years ranked by average from those who ranked all the songs.
     # 6. All years ranked by average of a specific user.
-    print("2 - All the years ranked by average.")
-    # 7. All the countries ranked by average.
-    # 8. All the countries ranked by average from those who ranked all the songs.
-    # 9. All the countries ranked by average of a specific user.
-    print("3 - All the countries ranked by average.")
-    # 10. All the entries from a specific year ranked.
-    # 11. All the entries from a specific year ranked from those who ranked all the songs.
-    # 12. All the entries from a specific year ranked from a specific user.
-    print("4 - All the entries from a specific year ranked.")
-    # 13. All the entries from a specific country ranked.
-    # 14. All the entries from a specific country ranked from those who ranked all the songs.
-    # 15. All the entries from a specific country ranked from a specific user.
-    print("5 - All the entries from a specific country ranked.")
+    # print("2 - All the years ranked by average.")
+    # # 7. All the countries ranked by average.
+    # # 8. All the countries ranked by average from those who ranked all the songs.
+    # # 9. All the countries ranked by average of a specific user.
+    # print("3 - All the countries ranked by average.")
+    # # 10. All the entries from a specific year ranked.
+    # # 11. All the entries from a specific year ranked from those who ranked all the songs.
+    # # 12. All the entries from a specific year ranked from a specific user.
+    # print("4 - All the entries from a specific year ranked.")
+    # # 13. All the entries from a specific country ranked.
+    # # 14. All the entries from a specific country ranked from those who ranked all the songs.
+    # # 15. All the entries from a specific country ranked from a specific user.
+    # print("5 - All the entries from a specific country ranked.")
     # 16. A specific entry's overall result.
     # 17. A specific entry's result on someone's ranking.
     # 18. List of a specific placement range of the ranking.
@@ -135,9 +139,39 @@ def main():
     
     # If statements that calls each function depending on the choice.
     if choice1 == 1:
+        # Print statement
+        print("\nWhere would you like to get the top 250 from?")
+        print("1 - All users")
+        print(f"2 - All users who ranked all {num_entries[edition]} songs")
+        # Due to the 2025 spreadsheet not containing the names of each user, only the organizer's ranking will be shown.
+        if edition == 2025:
+            print("3 - The organizer's top 250")
+        else:
+            print("3 - A specific user's top 250")
+
+        # Gets user input
+        choice2 = input("Type in the number associated with the choice you want: ")
+        
+        # Ensures that the user enters the number listed in the choices
+        while choice2 not in [str(c) for c in range(1, 4)]:
+            print("That is not a number listed. Try a number listed!")
+            print("Here are the choices again.\n")
+            choice2 = input("Type in the number associated with the choice you want: ")
+        # Else statement converts the choice into an int value.
+        else:
+            choice2 = int(choice2)
+
+        if choice2 == 1:
+            entire_ranking(edition)
+        elif choice2 == 2:
+            entire_ranking_all_songs(edition)
+
+    elif choice1 == 2:
+        # Print statement
         print("\nWhere would you like to get the ranking from?")
         print("1 - All users")
         print(f"2 - All users who ranked all {num_entries[edition]} songs")
+        # Due to the 2025 spreadsheet not containing the names of each user, only the organizer's ranking will be shown.
         if edition == 2025:
             print("3 - The organizer's ranking")
         else:
@@ -161,11 +195,11 @@ def main():
             entire_ranking_all_songs(edition)
     #     else:
     #         print_all_users()
-    # elif choice1 == 2:
-    #     all_year_average()
     # elif choice1 == 3:
-    #     all_country_average()
+    #     all_year_average()
     # elif choice1 == 4:
+    #     all_country_average()
+    # elif choice1 == 5:
     #     pass
 
     
